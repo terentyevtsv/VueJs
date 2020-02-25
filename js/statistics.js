2a2abe6c00ffa260a30f75510dd6ad8fc3e6bd64
Terentiev_SV <TerentievSV@samnipineft.ru>
2020-02-25 14:12:27 +0400

    Обернул модули

--- js/statistics.js
+++ js/statistics.js
@@ -1,22 +1,35 @@
-var CUSTOMERS_COLUMNS_COUNT = 3;
-var COLUMNS_OFFSET = 2;
+'use strict';
 
-var start = COLUMNS_OFFSET;
+(function () {
+  var CUSTOMERS_COLUMNS_COUNT = 3;
+  var COLUMNS_OFFSET = 2;
 
+  var start = COLUMNS_OFFSET;
 
-var Statistics = {
-  props: ['stages', 'customers'],
-  template: '#grid-template',
-  methods: {
-    customerStyleObject: function () {
-      var styleObject = {
-        'grid-column-start': start,
-        'grid-column-end': start + CUSTOMERS_COLUMNS_COUNT
-      };
+  window.Statistics = {
+    props: ['stages', 'customers'],
+    template: '#grid-template',
+    methods: {    
+      customerStyleObject: function () {
+        var styleObject = {
+          'grid-column-start': start,
+          'grid-column-end': start + CUSTOMERS_COLUMNS_COUNT
+        };
 
-      start += CUSTOMERS_COLUMNS_COUNT;
+        start += CUSTOMERS_COLUMNS_COUNT;
+        
+        return styleObject;
+      },
       
-      return styleObject;
-    }    
-  }
-};
+      stageValueStyleObject: function (customerIndex, stageIndex) {
+
+
+        var styleObject = {
+
+        };
+
+        return styleObject;
+      }
+    }
+  };
+})();
